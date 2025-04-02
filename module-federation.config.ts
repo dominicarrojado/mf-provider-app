0259147ed443ccd8ab5dea60efbc1d0b9030a816
Dominic Arrojado <dominicarrojado@gmail.com>
2025-04-03 00:33:32 +0800

    set up tailwindcss

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -3,10 +3,12 @@ import { createModuleFederationConfig } from '@module-federation/rsbuild-plugin'
 export default createModuleFederationConfig({
   name: 'mf_provider_app',
   exposes: {
-    '.': './src/components/ProviderComponent.tsx',
+    '.': './src/App.tsx',
   },
   shared: {
     react: { singleton: true },
     'react-dom': { singleton: true },
+    tailwindcss: { singleton: true },
+    '@tailwindcss/postcss': { singleton: true },
   },
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css';
-import Provider from './components/ProviderComponent';
 
-const App = () => {
+const App: React.FC = () => {
   return (
-    <div className="content">
-      <Provider />
+    <div className="flex h-screen flex-col items-center justify-center bg-gradient-to-r from-blue-600 via-indigo-600 to-purple-700">
+      <div className="relative mb-5 w-44">
+        <img
+          src="https://module-federation.io/svg.svg"
+          alt="logo"
+          className="h-[164px] w-auto"
+        />
+      </div>
+      <h1 className="mb-5 text-5xl text-white">Hello Module Federation 2.0</h1>
     </div>
   );
 };
