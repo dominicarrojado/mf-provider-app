ecc6c7a33a4ff9bc54f9d3e87ad391a257dbb2d9
Dominic Arrojado <dominicarrojado@gmail.com>
2025-04-05 13:20:57 +0800

    fix shared modules in module federation config

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -8,9 +8,13 @@ export default createModuleFederationConfig({
   shared: {
     react: { singleton: true },
     'react-dom': { singleton: true },
-    tailwindcss: { singleton: true },
-    '@tailwindcss/postcss': { singleton: true },
-    '@fontsource/roboto': { singleton: true },
-    '@fontsource/poppins': { singleton: true },
+    '@fontsource/poppins/500.css': { singleton: true },
+    '@fontsource/poppins/500-italic.css': { singleton: true },
+    '@fontsource/poppins/600.css': { singleton: true },
+    '@fontsource/poppins/600-italic.css': { singleton: true },
+    '@fontsource/roboto/400.css': { singleton: true },
+    '@fontsource/roboto/400-italic.css': { singleton: true },
+    '@fontsource/roboto/500.css': { singleton: true },
+    '@fontsource/roboto/500-italic.css': { singleton: true },
   },
 });
